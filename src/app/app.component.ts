252067726f5d20f9e33ec596b180bbbe3d33968c
Angela <angela.rod1994@gmail.com>
2024-08-20 19:01:52 -0500

    cambios 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
-
+import { ContainerComponent } from './components/container/container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,ContainerComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Bar_project-frondend';
+  title = 'Bar_project-frontend';
 }
