729e25b59e3f07f40c5ba2c593bfa8f72c50ff8c
Angela <angela.rod1994@gmail.com>
2024-08-29 19:29:09 -0500

    Cambios29agost

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { ServicesComponent } from './components/services/services.component';
+import { AnnouncementsComponent } from './components/announcements/announcements.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,CarouselComponent,AnnouncementsComponent,ServicesComponent,ContactsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Bar_project-frondend';
+  title = 'Bar_project-frontend';
 }
