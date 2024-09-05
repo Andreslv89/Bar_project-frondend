9f1523431ed8937a47d97dd9d40753e6250a260f
Angela <angela.rod1994@gmail.com>
2024-09-04 20:52:26 -0500

    cambiosv5_040924

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,12 @@ import { CandidateComponent } from './components/candidate/candidate.component';
 import { HomeComponent } from './components/home/home.component';
 import { SuscribeComponent } from './components/suscribe/suscribe.component';
 import { BeersComponent } from './components/beers/beers.component';
+import { ContactfooterComponent } from './components/contacts/contactfooter/contactfooter.component';
 
 export const routes: Routes = [
     {path:'reserve',component:ReserveComponent },
     {path:'candidate',component:CandidateComponent},
     {path: 'home', component: HomeComponent },
     {path:'suscribe',component:SuscribeComponent},
-    {path:'beers',component:BeersComponent}
-
+    {path:'beers',component:BeersComponent},
 ];
